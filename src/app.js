69620a0e744ec2504f536b582abc766cad64ec34
Thang Pham <thang2.pham@gmail.com>
2020-01-04 21:05:29 -0500

    Complete module 3

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ app.set("view engine", "ejs"); //embedded javascript engine
 //https://expressjs.com/en/starter/static-files.html       //middle-ware !
 app.use(express.static(path.join(__dirname, "/public")));
 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 // const accountData = fs.readFileSync('./src/json/accounts.json', "UTF8");
 // better practice to fully specify the path and file format
 const accountData = fs.readFileSync(
@@ -36,8 +39,7 @@ app.get("/", (req, res) => {
   //accounts is already a JSobject
   // res.render("index", { title: "Account Summary", accounts: accounts });
   //shortcut given same key/value
-  res.render("index", { title: "Account Summary", accounts});
-
+  res.render("index", { title: "Account Summary", accounts });
 });
 
 app.get("/profile", (req, res) => {
@@ -56,6 +58,59 @@ app.get("/checking", (req, res) => {
 app.get("/credit", (req, res) => {
   res.render("account", { account: accounts.credit });
 });
+
+app.get("/transfer", (req, res) => {
+  res.render("transfer");
+});
+
+app.post("/transfer", (req, res) => {
+  const data = req.body;
+  //Todo: check for negative, good data, etc.. ?  For the time being, short cut !
+  accounts[data.from].balance -= Number(data.amount);
+  accounts[data.to].balance += parseInt(data.amount, 10);  //Number doesn't work, trouble shoot later
+
+  // //If we need to break down into 2 steps as per instructions, it'd look like
+  // let tmpNewBalance = accounts[data.from].balance;
+  // // After some checkings, update tmpNewBalance
+  // tmpNewBalance -= data.amount;
+  // // Set new balance for FROM acct
+  // accounts[data.from].balance = tmpNewBalance;
+  // //repeat for ...TO
+
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "json", "accounts.json"),
+    accountsJSON,
+    "UTF8"
+  );
+
+  res.render("transfer", { message: "Transfer Completed" });
+});
+
+
+
+app.get("/payment", (req, res) => {
+  res.render("payment", {account: accounts.credit});
+});
+
+app.post("/payment", (req, res) => {
+  const data = req.body;
+  //Todo: check for negative, good data, etc.. ?  For the time being, short cut !
+  accounts["credit"].balance -= parseInt(data.amount, 10);
+  accounts["credit"].available += parseInt(data.amount, 10);
+
+  //Course suggests to use parseInt(), wouldn't this round it off ?
+
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "json", "accounts.json"),
+    accountsJSON,
+    "UTF8"
+  );
+
+  res.render("payment", { message: "Payment Successful", account: accounts.credit });
+});
+
 
 let port = process.env.PORT || 3000;
 
